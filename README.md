79204c732c69266fec0e8e3d54b318ed917d0f2b
Carlton Joseph <carlton.joseph@gmail.com>
2021-02-25 15:57:45 -0800

    rename hosting dir and add readme details

--- README.md
+++ README.md
@@ -1,5 +1,7 @@
 # Search Blog VSCode Extension
 
+Example VSCode Extension
+
 ## History
 
 Created by following:
@@ -11,4 +13,26 @@ Created by following:
 ```bash
 npm install -g yo generator-code
 yo code
+# add you extension to the code
+npm i -g vsce # tool for publishing extension
+```
+
+Publish the package by following the steps below.
+From
+https://code.visualstudio.com/api/working-with-extensions/publishing-extension
+
+1 - Login to Azure https://dev.azure.com/
+2 - Create a project (or select an old one)
+3 - Click upper right user icon and select personal access token PAT
+4 - Maker sure the "Organization" is set to "All accessible organizations"
+5 - Select "custom defined" scopes
+6 - At the end on the next page select "Show all scopes"
+7 - Select manage form under market place
+8 - Save the token in the .env file
+9 - Create a publisher by following the link on publishing-extension link noted above
+10 - note the ID next to the publisher - needed for login with the PAT
+
+```bash
+vsce login AppsForTracking
+vsce publish
 ```
--- extension.js
+++ extension.js
@@ -1,36 +1,36 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
-const vscode = require('vscode');
+const vscode = require("vscode");
+const fetch = require("node-fetch");
+const xmlParser = require("fast-xml-parser");
 
-// this method is called when your extension is activated
-// your extension is activated the very first time the command is executed
+async function activate(context) {
+  const res = await fetch("https://blog.webdevsimplified.com/rss.xml");
+  const xml = await res.text();
+  const json = xmlParser.parse(xml);
+  const articles = json.rss.channel.item.map(
+    ({ title, description, link }) => ({
+      label: title,
+      detail: description,
+      link,
+    })
+  );
+  console.log(articles);
+  let disposable = vscode.commands.registerCommand(
+    "search-blog-example.searchWdsBlog",
+    async function () {
+      const article = await vscode.window.showQuickPick(articles, {
+        matchOnDetail: true,
+      });
+      if (articles === null) return;
+      vscode.env.openExternal(article.link);
+    }
+  );
 
-/**
- * @param {vscode.ExtensionContext} context
- */
-function activate(context) {
-
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "search-blog-example" is now active!');
-
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with  registerCommand
-	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('search-blog-example.helloWorld', function () {
-		// The code you place here will be executed every time your command is executed
-
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from Search Blog Example!');
-	});
-
-	context.subscriptions.push(disposable);
+  context.subscriptions.push(disposable);
 }
 
-// this method is called when your extension is deactivated
 function deactivate() {}
 
 module.exports = {
-	activate,
-	deactivate
-}
+  activate,
+  deactivate,
+};
